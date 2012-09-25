b126c2f6ece46182fa1f2a54989f72a603085fd8
josephfr <josephfr@andrew.cmu.edu>
2012-09-24 22:27:41 -0400

    pasted in JT's code, then the TurnTo loop. Added GUI stuff.

--- lab2/src/edu/cmu/ri/mrpl/example/SampleRobotApp.java
+++ lab2/src/edu/cmu/ri/mrpl/example/SampleRobotApp.java
@@ -1,3 +1,5 @@
+// XXX TODO add in error correction
+
 package edu.cmu.ri.mrpl.example;
 /*
 
@@ -31,7 +33,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 
 	private Robot robot;
 	private SonarConsole sc;
-	
+
 	private JFrame scFrame;
 
 	private JButton connectButton;
@@ -41,22 +43,26 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 	private JButton waitButton;
 	private JButton turnToButton;
 	private JButton goToButton;
+	private JButton poseToButton;
 	private JFormattedTextField argumentField;
 	private JFormattedTextField remainingField;
+	private JFormattedTextField xField;
+	private JFormattedTextField yField;
+	private JFormattedTextField thField;
 
 	private JButton stopButton;
 	private JButton quitButton;
 	private JButton executeButton;
-	
+
 	private java.util.Queue<Task> upcomingTasks;
-	
+
 	static final int DEFAULT_ROOM_SIZE = 4;
-	
+
 	private Task curTask = null;
 
 	public SampleRobotApp() {
 		super("Kick Ass Sample Robot App");
-		
+
 		upcomingTasks = new LinkedList<Task>();
 
 		connectButton = new JButton("connect");
@@ -66,12 +72,19 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 		waitButton = new JButton("Wait!");
 		turnToButton = new JButton("Turn to angle!");
 		goToButton = new JButton("Go to distance!");
+		poseToButton = new JButton("Go to pose!");
 		argumentField = new JFormattedTextField(NumberFormat.getInstance());
 		argumentField.setValue(1);
 		NumberFormat nf = NumberFormat.getInstance();
 		nf.setMaximumFractionDigits(2);
 		remainingField = new JFormattedTextField(nf);
 		remainingField.setEditable(false);
+		xField = new JFormattedTextField(NumberFormat.getInstance());
+		xField.setValue(1);
+		yField = new JFormattedTextField(NumberFormat.getInstance());
+		yField.setValue(1);
+		thField = new JFormattedTextField(NumberFormat.getInstance());
+		thField.setValue(1);
 		stopButton = new JButton(">> stop <<");
 		quitButton = new JButton(">> quit <<");
 		executeButton = new JButton("Execute File");
@@ -83,6 +96,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 		waitButton.addActionListener(this);
 		turnToButton.addActionListener(this);
 		goToButton.addActionListener(this);
+		poseToButton.addActionListener(this);
 		stopButton.addActionListener(this);
 		quitButton.addActionListener(this);
 		executeButton.addActionListener(this);
@@ -120,21 +134,29 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 		box.add(Box.createHorizontalStrut(30));
 		box.add(goToButton);
 		box.add(Box.createHorizontalStrut(30));
+		box.add(poseToButton);
+		box.add(Box.createHorizontalStrut(30));
 
 		main.add(Box.createVerticalStrut(30));
-		
+
 		box = Box.createHorizontalBox();
 		main.add(box);
 		box.add(executeButton);
 
 		main.add(Box.createVerticalStrut(30));
-		
+
 		box = Box.createHorizontalBox();
 		main.add(box);
 		box.add(Box.createHorizontalStrut(30));
 		box.add(argumentField);
 		box.add(Box.createHorizontalStrut(30));
 		box.add(remainingField);
+		box.add(Box.createHorizontalStrut(30));
+		box.add(xField);
+		box.add(Box.createHorizontalStrut(30));
+		box.add(yField);
+		box.add(Box.createHorizontalStrut(30));
+		box.add(thField);
 		box.add(Box.createHorizontalStrut(30));
 
 		main.add(Box.createVerticalStrut(30));
@@ -146,7 +168,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 		box.add(quitButton);
 
 		main.add(Box.createVerticalStrut(30));
-		
+
 		this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
@@ -166,7 +188,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 		sc = new SonarConsole();
 		scFrame.add(sc);
 		scFrame.pack();
-		
+
 		this.setVisible(true);
 
 		String message = "Is this running on the robot?";
@@ -177,7 +199,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 		else {
 			robot = new SimRobot();
 		}
-		
+
 		robot.setAccel(0.25, 0.25);
 	}
 
@@ -258,19 +280,22 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			Task next = upcomingTasks.remove();
 			System.err.println("attempting to start " + next);
 			//if (canStart(next)) {
-				new Thread(next).start();
+			new Thread(next).start();
 			//}
 		}
 		else {
 			System.err.println("no more tasks left or task is currently running");
 		}
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
-		
+
 		double argument = Double.parseDouble(argumentField.getText());
-		
+		double x = Double.parseDouble(xField.getText());
+		double y = Double.parseDouble(yField.getText());
+		double th = Double.parseDouble(thField.getText());
+
 		if (source==connectButton) {
 			connect();
 		} else if ( source==disconnectButton ) {
@@ -291,6 +316,9 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 		} else if ( source==goToButton ) {
 			upcomingTasks.add(new GoToTask(this, argument));
 			startUpcomingTasks();
+		} else if (source == poseToButton) {
+			upcomingTasks.add(new PoseToTask(this, x, y, th));
+			startUpcomingTasks();
 		} else if ( source==executeButton ) {
 			addFileTasksToQueue();
 		}
@@ -299,22 +327,22 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 	public void addFileTasksToQueue() {
 		JFileChooser chooser = new JFileChooser();
 		int returnVal = chooser.showOpenDialog(this);
-	    if(returnVal == JFileChooser.APPROVE_OPTION) {
-	       System.out.println("You chose to open this file: " +
-	            chooser.getSelectedFile().getName());
-	    }
-	    
-	    CommandSequence commandSequence = new CommandSequence();
-	    
-	    try {
-	    	commandSequence.readFile(chooser.getSelectedFile().getPath());
+		if(returnVal == JFileChooser.APPROVE_OPTION) {
+			System.out.println("You chose to open this file: " +
+					chooser.getSelectedFile().getName());
+		}
+
+		CommandSequence commandSequence = new CommandSequence();
+
+		try {
+			commandSequence.readFile(chooser.getSelectedFile().getPath());
 		} catch (IOException e) {
 			System.err.println("Couldn't read file");
 		}
 
-	    executeActionList(commandSequence);
+		executeActionList(commandSequence);
 	}
-	
+
 	// TODO allow more than one task to run?
 	public synchronized boolean canStart(Task t) {
 		if (curTask!=null)
@@ -341,7 +369,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			}
 		});
 	}
-	
+
 	public void executeActionList (ArrayList<Command> commands) {
 		for (Command cmd : commands) {
 			System.err.println(cmd.type);
@@ -349,38 +377,38 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			case PAUSE:
 				upcomingTasks.add(new PauseTask(this));
 				break;
-			
+
 			case TURNTO:
 				upcomingTasks.add(new TurnToTask(this,
-					((Command.AngleArgument) cmd.argument).angle.angleValue()));
+						((Command.AngleArgument) cmd.argument).angle.angleValue()));
 				break;
-			
+
 			case GOTO:
 				upcomingTasks.add(new GoToTask(this,
-					((Command.LengthArgument) cmd.argument).d));
+						((Command.LengthArgument) cmd.argument).d));
 				break;
-				
+
 			case WAIT:
 				upcomingTasks.add(new WaitTask(this,
-					((Command.LengthArgument) cmd.argument).d));
+						((Command.LengthArgument) cmd.argument).d));
 				break;
-				
+
 			default:
 				System.err.println("unimplemented Command type: " + cmd.type);
 				new Speech().speak("unimplemented Command type: " + cmd.type);
 				break;
 			}
 		}
-		
+
 		startUpcomingTasks();
 	}
 
 	// here we implement the different robot controllers
 	// as Tasks
 	//
-	
+
 	class PauseTask extends Task implements KeyListener, MouseListener {
-		
+
 		Speech speech;
 		boolean done;
 
@@ -406,7 +434,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 					System.err.println("pause sleep interrupted");
 				}
 			}
-			
+
 			long endTime = System.currentTimeMillis();
 			double elapsedTime = (endTime - startTime) / 1000.0;
 			remainingField.setValue(elapsedTime);
@@ -421,7 +449,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			pauseButton.removeKeyListener(this);
 			SampleRobotApp.this.removeKeyListener(this);
 		}
-		
+
 		public void mouseClicked(MouseEvent arg0) {
 			done = true;
 			SampleRobotApp.this.removeMouseListener(this);
@@ -434,7 +462,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 		public void mousePressed(MouseEvent arg0) {}
 		public void mouseReleased(MouseEvent arg0) {}
 	}
-	
+
 	class WaitTask extends Task {
 
 		Speech speech;
@@ -445,7 +473,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			super(tc);
 			setDuration(d);
 		}
-		
+
 		public void setDuration (double d) {
 			duration = d;
 		}
@@ -455,7 +483,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			speech.speak("Waiting " + AngleMath.roundTo2(duration) + " seconds");
 			startTime = System.currentTimeMillis();
 			double elapsed = elapsedTime();
-			
+
 			while(!shouldStop() && elapsed < duration) {
 				elapsed = elapsedTime();
 				if (elapsed >= duration) {
@@ -464,7 +492,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 				try {
 					// XXX is this the right place to update this box?
 					remainingField.setValue(duration - elapsed);
-					
+
 					Thread.sleep(5);
 				} catch(InterruptedException iex) {
 					System.err.println("wait sleep interrupted");
@@ -474,7 +502,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			remainingField.setText(duration - elapsed + "");
 		}
 
- 		private double elapsedTime() {
+		private double elapsedTime() {
 			return (System.currentTimeMillis() - startTime) / 1000.0;
 		}
 
@@ -484,7 +512,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 	}
 
 	class TurnToTask extends Task {
-		
+
 		Speech speech;
 		double desiredAngle;
 		Controller controller;
@@ -493,7 +521,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			super(tc);
 			setDesiredAngle(a);
 		}
-		
+
 		public void setDesiredAngle (double a) {
 			desiredAngle = a;
 			//System.out.println("desiredAngle: " + desiredAngle);
@@ -513,13 +541,13 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			double destAngle = Angle.normalize(curAngle + desiredAngle);
 			//System.out.println("destAngle: " + destAngle);
 			double angleErr = Angle.normalize(destAngle - curAngle);
-			
+
 			final double ANGLE_TOLERANCE = 0.01;
 			final double SPEED_TOLERANCE = 0.01;
 
 			while(!shouldStop() &&
 					(abs(angleErr) > ANGLE_TOLERANCE
-					|| abs(robot.getVelLeft()) > SPEED_TOLERANCE))
+							|| abs(robot.getVelLeft()) > SPEED_TOLERANCE))
 			{
 				//System.out.println("angleErr = " + angleErr);
 				robot.updateState();
@@ -529,7 +557,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 				//curTime = System.nanoTime();
 				curTime = System.currentTimeMillis();
 				angleErr = Angle.normalize(destAngle - curAngle);
-				
+
 				double angleTraveled = Angle.normalize(curAngle - lastAngle);
 				boolean progressMade = signum(angleTraveled) == signum(angleErr);
 
@@ -544,14 +572,14 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 				//System.err.println(angleErr + " off, speed = " + u*direction);
 				double speed = direction*u;
 				controller.setVel(-speed, speed);
-				
+
 				try {
 					Thread.sleep(5);
 				} catch(InterruptedException iex) {
 					System.err.println("turn-to sleep interrupted");
 				}
 			}
-			
+
 			// TODO error should be in milliradians?
 			speech.speak("error " + AngleMath.roundTo2(angleErr*1000) + " milliradians");
 			//System.out.println("curAngle: " + curAngle);
@@ -564,7 +592,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 	}
 
 	class GoToTask extends Task {
-		
+
 		double desiredDistance;
 		RealPose2D robotStartedHere;
 		Controller controller;
@@ -575,7 +603,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			super(tc);
 			setDesiredDistance(d);
 		}
-		
+
 		public void setDesiredDistance (double d) {
 			controller = new Controller(robot);
 			perceptor = new Perceptor(robot);
@@ -598,13 +626,13 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			double lastTime = curTime;
 			final Point2D destPointRelStart = new Point2D.Double(desiredDistance, 0);
 			double distanceErr = destPointRelStart.getX() - curPoseRelStart.getX();
-			
+
 			final double DISTANCE_TOLERANCE = 0.005;
 			final double SPEED_TOLERANCE = 0.01;
 
 			while(!shouldStop() &&
 					(abs(distanceErr) > DISTANCE_TOLERANCE
-					|| abs(robot.getVelLeft()) > SPEED_TOLERANCE))
+							|| abs(robot.getVelLeft()) > SPEED_TOLERANCE))
 			{
 				robot.updateState();
 				lastPoseRelStart = curPoseRelStart;
@@ -612,7 +640,7 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 				curPoseRelStart = perceptor.getRelPose(robotStartedHere);
 				curTime = System.currentTimeMillis();
 				distanceErr = destPointRelStart.getX() - curPoseRelStart.getX();
-				
+
 				double distanceTraveled = curPoseRelStart.getX() - lastPoseRelStart.getX();
 				boolean progressMade = signum(distanceTraveled) == signum(distanceErr);
 
@@ -620,19 +648,19 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 				double dterm = Kd*(abs(distanceTraveled))/(curTime-lastTime);
 				if (progressMade) {
 				}
-					dterm *= -1;
+				dterm *= -1;
 				//System.err.println("pterm = " + pterm + "\ndterm = " + dterm + "\n");
 				u = pterm + dterm;
 				double speed = u;
 				controller.setVel(speed, speed);
-				
+
 				try {
 					Thread.sleep(5);
 				} catch(InterruptedException iex) {
 					System.err.println("go-to sleep interrupted");
 				}
 			}
-			
+
 			speech.speak("error " + AngleMath.roundTo2(distanceErr*1000) + " millimeters");
 			controller.stop();
 		}
@@ -641,6 +669,170 @@ public class SampleRobotApp extends JFrame implements ActionListener, TaskContro
 			return "go-to task: " + desiredDistance;
 		}
 	}
-	
+
+	class PoseToTask extends Task {
+		RealPose2D desiredPose;
+		RealPose2D robotStartedHere;
+		Controller controller;
+		Perceptor perceptor;
+		Speech speech;
+
+		PoseToTask(TaskController tc, double x, double y, double th) {
+			super(tc);
+			setDesiredPose(x,y,th);
+		}
+
+		public void setDesiredPose (double x, double y, double th) {
+			controller = new Controller(robot);
+			perceptor = new Perceptor(robot);
+			desiredPose = new RealPose2D(x,y,th);
+		}
+
+		// setDesiredDistance should be called before calling this method
+		public void taskRun() {
+			speech = new Speech();
+			robot.updateState();
+			// XXX changed this from 1 to 2
+			double Kp = 2;
+			double Kd = 10;
+			double u = 0;
+
+			robotStartedHere = perceptor.getWorldPose();
+			RealPose2D curPoseRelStart = perceptor.getRelPose(robotStartedHere);
+			RealPose2D lastPoseRelStart = curPoseRelStart;
+			double curTime = System.nanoTime();
+			double lastTime = curTime;
+			final RealPose2D destPoseRelStart = desiredPose;
+			double distanceErr;
+
+			double destR = (pow(destPoseRelStart.getX(),2) +
+					pow(destPoseRelStart.getY(),2))/(destPoseRelStart.getY()/2);
+			double curv = 1.0/destR;
+			double destArcAngle;
+			if(destR >= 0)
+				destArcAngle = atan2(destPoseRelStart.getX(), destR -
+						destPoseRelStart.getY());
+			else
+				destArcAngle = atan2(destPoseRelStart.getX(), -destR +
+						destPoseRelStart.getY());
+
+			double dist = destR*destArcAngle;
+			distanceErr = dist;
+
+
+			System.out.println("destR= "+destR+", dist= "+dist
+					+", destArcAngle="+destArcAngle);
+
+			final double DISTANCE_TOLERANCE = 0.01;
+			double SPEED_TOLERANCE = 0.01;
+
+			while(!shouldStop() &&
+					(abs(distanceErr) > DISTANCE_TOLERANCE
+							|| abs(robot.getVelLeft()) > SPEED_TOLERANCE))
+			{
+				robot.updateState();
+				lastPoseRelStart = curPoseRelStart;
+				lastTime = curTime;
+				curPoseRelStart = perceptor.getRelPose(robotStartedHere);
+				curTime = System.currentTimeMillis();
+
+				double curArcAngle;
+				if(destR >= 0)
+					curArcAngle = atan2(curPoseRelStart.getX(), destR -
+							curPoseRelStart.getY());
+				else
+					curArcAngle = atan2(curPoseRelStart.getX(), -destR +
+							curPoseRelStart.getY());
+
+				distanceErr = destR*(destArcAngle-curArcAngle);
+
+				System.out.println("destR= "+destR+", distanceErr= "+distanceErr
+						+", curArcAngle="+curArcAngle);
+
+				double distanceTraveled = dist - distanceErr;
+				boolean progressMade = signum(distanceTraveled) == signum(distanceErr);
+
+				double pterm = Kp*distanceErr;
+				double dterm = Kd*(abs(distanceTraveled))/(curTime-lastTime);
+				if (progressMade) {
+				}
+				dterm *= -1;
+				System.err.println("pterm = " + pterm + "\ndterm = " + dterm + "\n");
+				u = pterm + dterm;
+				double speed = u;
+				controller.setCurvVel(curv, speed);
+
+				try {
+					Thread.sleep(5);
+				} catch(InterruptedException iex) {
+					System.err.println("go-to sleep interrupted");
+				}
+			}
+
+			// XXX copied code from TurnToTask.
+			// Check whether the "rapid transition" rule is broken here.
+			Kp = .4;
+			Kd = 95;
+			u = 0;
+			robot.updateState();
+			double curAngle = Angle.normalize(robot.getHeading());
+			double lastAngle = curAngle;
+			curTime = System.nanoTime();
+			lastTime = curTime;
+			curPoseRelStart = perceptor.getRelPose(robotStartedHere);
+			double desiredAngle = desiredPose.getTh() - curPoseRelStart.getTh();
+			double destAngle = Angle.normalize(curAngle + desiredAngle);
+			//System.out.println("destAngle: " + destAngle);
+			double angleErr = Angle.normalize(destAngle - curAngle);
+
+			final double ANGLE_TOLERANCE = 0.01;
+			SPEED_TOLERANCE = 0.01;
+
+			while(!shouldStop() &&
+					(abs(angleErr) > ANGLE_TOLERANCE
+							|| abs(robot.getVelLeft()) > SPEED_TOLERANCE))
+			{
+				//System.out.println("angleErr = " + angleErr);
+				robot.updateState();
+				lastAngle = curAngle;
+				lastTime = curTime;
+				curAngle = Angle.normalize(robot.getHeading());
+				//curTime = System.nanoTime();
+				curTime = System.currentTimeMillis();
+				angleErr = Angle.normalize(destAngle - curAngle);
+
+				double angleTraveled = Angle.normalize(curAngle - lastAngle);
+				boolean progressMade = signum(angleTraveled) == signum(angleErr);
+
+				double pterm = Kp*abs(angleErr);
+				double dterm = Kd*(abs(angleTraveled))/(curTime-lastTime);
+				if (progressMade) {
+					dterm *= -1;
+				}
+				//System.err.println("pterm = " + pterm + "\ndterm = " + dterm + "\n");
+				u = pterm + dterm;
+				double direction = signum(angleErr);
+				//System.err.println(angleErr + " off, speed = " + u*direction);
+				double speed = direction*u;
+				controller.setVel(-speed, speed);
+
+				try {
+					Thread.sleep(5);
+				} catch(InterruptedException iex) {
+					System.err.println("pose-to (turn) sleep interrupted");
+				}
+			}
+
+			// TODO speak error aloud
+			speech.speak("remaining error " + AngleMath.roundTo2(distanceErr*1000)
+					+ " millimeters");
+			controller.stop();
+		}
+
+		public String toString() {
+			return "pose-to task: " + desiredPose;
+		}
+	}
+
 	private static final long serialVersionUID = 0;
 }
